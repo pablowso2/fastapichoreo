adf362ba5ffb5de83b20928dbfc98ca26a84134e
pablowso2 <pablosa@wso2.com>
2024-06-19 16:30:31 +0200

    initial

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/items/")
+async def read_item():
+    return ["1", "2","3"]
+
